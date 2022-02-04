248863d099c3bbe85491bf81191bd66645e362b1
teatimehr13 <a5432158086@gmail.com>
2022-02-04 16:14:36 +0800

    adjust photo display method on backside

--- back/me.php
+++ back/me.php
@@ -1,7 +1,7 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli"><?=$DB->title;?></p>
-    <form method="post"  action="api/edit.php?do=<?=$DB->table;?>">
-    <table width="100%">
+    <p class="t cent botli"><?= $DB->title; ?></p>
+    <form method="post" action="api/edit.php?do=<?= $DB->table; ?>">
+        <table width="100%">
             <tbody>
                 <tr class="yel">
                     <td width="20%">圖片</td>
@@ -12,36 +12,44 @@
                     <td></td>
                 </tr>
                 <?php
-                $rows=$DB->all();
-                foreach($rows as $row){
-                    $checked=($row['sh']==1)?'checked':'';
+                $rows = $DB->all();
+                foreach ($rows as $row) {
+                    $checked = ($row['sh'] == 1) ? 'checked' : '';
                 ?>
-                <tr>
-                    <td width="20%">
-                        <img src="./img/<?=$row['img'];?>" style="width:120px;height:100px">
-                    </td>
-                    <td width="23%">
-                        <textarea name="text[]"><?=$row['text'];?></textarea>
-                    </td>
+                    <tr>
+                        <td width="20%">
+                            <?php if (empty($row['img'])) {
+                                echo "<img src='https://fakeimg.pl/120x100/?text=No Img'>";
+                            } else { ?>
+                                <img src="./img/<?= $row['img']; ?>" style="width:120px;height:100px">
+                            <?php
+                            }
+                            ?>
+                        </td>
 
-                    <td width="10%">
-                        <input type="text" name="ordered[]" value="<?=$row['ordered'];?>" style="width: 50px;">
-                    </td>
+                        <!-- <td width="20%">
+                        <img src="./img/<?= $row['img']; ?>" style="width:120px;height:100px">
+                    </td> -->
+                        <td width="23%">
+                            <textarea name="text[]"><?= $row['text']; ?></textarea>
+                        </td>
 
-                    <td width="7%">
-                        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=$checked;?>>
-                    </td>
-                    <td width="7%">
-                        <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                        <td width="10%">
+                            <input type="text" name="ordered[]" value="<?= $row['ordered']; ?>" style="width: 50px;">
+                        </td>
 
-                    </td>
-                    <td>
-                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
-                    <input type="button"
-                            onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/upload.php?do=<?=$DB->table;?>&id=<?=$row['id'];?>&#39;)" 
-                              value="更新圖片">
-                    </td>
-                </tr>
+                        <td width="7%">
+                            <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= $checked; ?>>
+                        </td>
+                        <td width="7%">
+                            <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+
+                        </td>
+                        <td>
+                            <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                            <input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/upload.php?do=<?= $DB->table; ?>&id=<?= $row['id']; ?>&#39;)" value="更新圖片">
+                        </td>
+                    </tr>
                 <?php
                 }
                 ?>
@@ -51,12 +59,10 @@
             <tbody>
                 <tr>
                     <td width="200px">
-                        <input type="button"
-                            onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/<?=$DB->table;?>.php?table=<?=$DB->table;?>&#39;)" 
-                              value="<?=$DB->button;?>">
+                        <input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/<?= $DB->table; ?>.php?table=<?= $DB->table; ?>&#39;)" value="<?= $DB->button; ?>">
                     </td>
                     <td class="cent">
-                        
+
                         <input type="submit" value="修改確定">
                         <input type="reset" value="重置">
                     </td>
