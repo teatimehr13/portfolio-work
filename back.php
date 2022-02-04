8dbca976af2f4a6579a141c7c28475c8031a839f
teatimehr13 <a5432158086@gmail.com>
2022-02-04 23:22:15 +0800

    revised php include on backside

--- back.php
+++ back.php
@@ -1,18 +1,13 @@
 ﻿<?php
 include_once "./base.php";
 
-
-if(!isset($_SESSION['login'])){ 
+if (!isset($_SESSION['login'])) {
     to("index.php");
     exit();
 }
 ?>
 
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!-- saved from url=(0068)?do=admin&redo=title -->
-<html xmlns="http://www.w3.org/1999/xhtml">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<!DOCTYPE html>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -21,81 +16,45 @@ if(!isset($_SESSION['login'])){
     <link href="./css/css.css" rel="stylesheet" type="text/css">
     <script src="./js/jquery-1.9.1.min.js"></script>
     <script src="./js/js.js"></script>
-   <link rel="stylesheet" href="./scss/bootstrap.css">
-    <!-- <link rel="stylesheet" href="./style2 copy.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">-->
+    <link rel="stylesheet" href="./scss/bootstrap.css">
+    <!-- <link rel="stylesheet" href="./style2 copy.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <style>
-.btn-purple {
-    color: #fff;
-    background-color: #c89fa3;
-    border-color: #c89fa3;
-}
-.btn-purple:hover,
-.btn-purple:focus,
-.btn-purple:active:hover {
-    color: #fff;
-    background-color:  #b5838d;
-    border-color: #b5838d;
-}
-a:hover{
-    text-decoration: none;
-}
+    .btn-purple {
+        color: #fff;
+        background-color: #c89fa3;
+        border-color: #c89fa3;
+    }
+
+    .btn-purple:hover,
+    .btn-purple:focus,
+    .btn-purple:active:hover {
+        color: #fff;
+        background-color: #b5838d;
+        border-color: #b5838d;
+    }
+
+    a:hover {
+        text-decoration: none;
+    }
 </style>
 
 <body>
-    <div id="cover" style="display:none; ">
-        <div id="coverr">
-            <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;"
-                onclick="cl(&#39;#cover&#39;)">X</a>
-            <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
-        </div>
-    </div>
+    <?php include_once "back/ajaxButton.php"; ?>
 
     <div id="main">
-<?php include "back/header.php";?>
+        <?php include "back/header.php"; ?>
         <div id="ms">
-            <div id="lf" style="float:left;">
-                <div id="menuput" class="border border-secondary">
-                    <!--主選單放此-->
-                    <a href="?do=title">
-                        <button type="button" class="btn btn-purple btn-block my-2">HEADER</button>
-                    </a>
 
-                    <a href="?do=me">
-                        <button type="button" class="btn btn-purple btn-block my-2">ME</button>
-                    </a>
+            <?php include_once "back/sideNav.php"; ?>
 
-                    <a href="?do=collection">
-                        <button type="button" class="btn btn-purple btn-block my-2">COLLECTION</button>
-                    </a>
-
-                    <a href="?do=skills">
-                        <button type="button" class="btn btn-purple btn-block my-2">SKILLS</button>
-                    </a>
-
-                    <a href="?do=bottom">
-                        <button type="button" class="btn btn-purple btn-block my-2">FOOTER</button>
-                    </a>
-
-                    <a href="?do=admin">
-                        <button type="button" class="btn btn-purple btn-block my-2">USERS</button>
-                    </a>
-
-
-                </div>
-                <div class="border border-secondary" style="margin:3px; width:100%; height:20%; line-height:100px;">
-                    <span class="t">進站總人數 :<?=$Total->find(1)['total'];?></span>
-                </div>
-            </div>
-            <div class="di"
-                style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
+            <div class="di" style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
                 <!--正中央-->
                 <table width="100%">
                     <tbody>
                         <tr>
-                            <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;"
-                                class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
+                            <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;" class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
                             </td>
                             <td><a href="logout.php"><button style="width:100%; height:50px;">管理登出</button></a></td>
                         </tr>
@@ -103,25 +62,21 @@ a:hover{
                 </table>
                 <?php
 
-							$do=isset($_GET["do"])?$_GET["do"]:'title';
-							$file="./back/".$do.".php";
-							if(file_exists($file)){
-								include $file;
-							}else{
-								//echo "檔案不存在";
-								include "./back/title.php";
-							}	
+                $do = isset($_GET["do"]) ? $_GET["do"] : 'title';
+                $file = "./back/" . $do . ".php";
+                if (file_exists($file)) {
+                    include $file;
+                } else {
+                    //echo "檔案不存在";
+                    include "./back/title.php";
+                }
 
-								?>
+                ?>
             </div>
 
         </div>
         <div style="clear:both;"></div>
-        <div
-            style="width:1021px; left:0px; position:relative; background:#c9ada7; margin-top:4px; height:123px; display:block;">
-            <span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
-            <!-- <span><i class="fab fa-linkedin-in"></i></span> -->
-        </div>
+        <?php include_once "./back/footer.php" ?>
     </div>
 
 </body>
